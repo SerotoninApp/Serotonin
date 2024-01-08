a650d22c62e54477d1544440cb0cbe600fdea9bc
Mineek <mineek@mineek.nl>
2024-01-08 15:04:56 +0100

    fix the springboardshim on arm64

--- RootHelperSample/launchdshim/SpringBoardShim/build.sh
+++ RootHelperSample/launchdshim/SpringBoardShim/build.sh
@@ -5,8 +5,8 @@ function replaceByte() {
 make
 # /Users/ibarahime/insert_dylib/insert_dylib/insert_dylib /var/jb/usr/lib/ellekit/libinjector.dylib .theos/obj/debug/arm64e/springboardshim springboardshiminjected --all-yes
 # /Users/ibarahime/insert_dylib/insert_dylib/insert_dylib /var/jb/usr/lib/libellekit.dylib springboardshiminjected springboardshiminjected --all-yes
-/Users/ibarahime/insert_dylib/insert_dylib/insert_dylib @loader_path/springboardhook.dylib .theos/obj/debug/arm64e/springboardshim springboardshiminjected --all-yes
+insert_dylib @loader_path/springboardhook.dylib .theos/obj/debug/arm64/springboardshim springboardshiminjected --all-yes
 
 # replaceByte 'springboardshiminjected' 8 
 ldid -SSpringBoardEnts.plist springboardshiminjected
-/Users/ibarahime/ChOma/ct_bypass -i springboardshiminjected -r -o springboardshimsignedinjected
+ct_bypass -i springboardshiminjected -r -o springboardshimsignedinjected
