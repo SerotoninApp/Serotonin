fc7bb9e7b927827cb12b3b6b388feb44c18fd063
BomberFish <bomberfish@bomberfish.ca>
2024-01-08 10:49:08 -0500

    pluh

--- usprebooter/usprebooterApp.swift
+++ usprebooter/usprebooterApp.swift
@@ -3,6 +3,12 @@ import SwiftUI
 struct usprebooterApp: App {
     @State var useNewUI: Bool = true
     init() {
+        if !FileManager.default.fileExists(atPath: "/var/mobile/Serotonin.jp2") {
+            if let bootlogo = Bundle.main.url(forResource: "Serotonin", withExtension: "jp2") {
+                try? FileManager.default.copyItem(at: bootlogo, to: URL(fileURLWithPath: "/var/mobile/Serotonin.jp2"))
+            }
+        }
+        
         if !FileManager.default.fileExists(atPath: "/var/mobile/boot-happy.jp2") {
             if let bootlogo = Bundle.main.url(forResource: "boot-happy", withExtension: "jp2") {
                 try? FileManager.default.copyItem(at: bootlogo, to: URL(fileURLWithPath: "/var/mobile/boot-happy.jp2"))
